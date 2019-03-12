b3b52c13389204c5eb0befae8f750e4f44f25806
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-12 13:11:40 -0400

    Changed button color to red using context system

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,23 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
-  render() {
+  renderButton(color) {
     return (
-      <button className="ui button primary">
+      <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
           {value => (value === "english" ? "Submit" : "Подать")}
         </LanguageContext.Consumer>
       </button>
+    );
+  }
+
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
