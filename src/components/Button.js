ada5413e9f972001262d3b28997ed4a7776b5fb9
Julia <Julia@Julias-MacBook-Pro.local>
2019-03-12 12:57:17 -0400

    Changed out context.type approach for Consumer component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,14 @@ import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
-
   render() {
-    const text = this.context === "english" ? "Submit" : "Подать";
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Подать")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
